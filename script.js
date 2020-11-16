271e3f10a4d8d8e1240ef44640d07729c464256d
nerangel <quentindtouroul@gmail.com>
2020-11-16 20:01:35 +0100

    fin

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* DOMs */
+
+
 var footer = document.getElementsByTagName("footer")[0];
 var hamburger = document.getElementsByClassName("navbar-toggler")[0];
 var cards = document.getElementsByClassName("card");
@@ -6,9 +9,10 @@ var editCardTwo = document.getElementsByClassName("btn-sm btn-outline-secondary"
 var navBar = document.getElementsByTagName("header")[0];
 var leftJumbotron = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-secondary")[0];
 var rightJumbotron = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-primary")[0];
-var rowCard = document.getElementsByClassName("row");
 var colCards = document.getElementsByClassName("col-md-4");
 var logo = document.getElementsByClassName("navbar-brand")[0];
+
+/* EVENEMENTS */
 
 var clickCounter = 0;
 var onFooterClick = function () {
@@ -44,8 +48,6 @@ var doubleClickOnNavbar = function() {
 };
 
 var overCards = function(index) {
-  console.log("Test")
-  console.log(index)
   cards[index].getElementsByTagName("img")[0].style = "width: 20%";
   cards[index].getElementsByClassName("card-text")[0].classList.add("collapse");
 }
@@ -63,6 +65,23 @@ var previousCard = function(){
   colCards[0].parentNode.insertBefore(colCards[0], colCards[colCards.length - 1].nextSibling)
 }
 
+var onLeft = function(){
+  document.getElementsByTagName("body")[0].classList = "col-4";
+}
+
+var onMiddle = function(){
+  document.getElementsByTagName("body")[0].classList = "col-4 offset-md-4";
+}
+
+var onRight = function(){
+  document.getElementsByTagName("body")[0].classList = "col-4 offset-md-8";
+}
+
+var resetBody = function(){
+  document.getElementsByTagName("body")[0].classList = "";
+}
+
+/* EVENEMENTS LIES AU DOM */
 footer.addEventListener('click', onFooterClick);
 hamburger.addEventListener("click", onHamburgerClick);
 editCardOne.addEventListener("click", onEditCardOne);
@@ -81,3 +100,14 @@ rightJumbotron.addEventListener("click", function(event) {
   event.preventDefault();
 });
 rightJumbotron.addEventListener("click", previousCard );
+logo.addEventListener("keydown", function(event) {
+  if (event.keyCode === 65) {
+    onLeft();
+  } else if (event.keyCode == 89) {
+    onMiddle();
+  } else if (event.keyCode == 80) {
+    onRight();
+  } else if (event.keyCode == 66) {
+    resetBody();
+  };
+});
